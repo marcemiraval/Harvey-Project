39cc1dbde551422b0a2320ba21f5c3d6a08f7209
marcemiraval <marcela.suarez@gmail.com>
2018-12-01 22:46:30 -0800

    Before I was clustering terms, now I'm clustering documents (tweets)
    Add code to create a table with top terms in each cluster
    
    A whole realization!

--- Chapter1/Colorado/ICC_Colorado.R
+++ Chapter1/Colorado/ICC_Colorado.R
@@ -25,9 +25,9 @@ library(cluster) # For silhouette coefficient
 
 
 library(lsa) # To compute cosine metric
-library(ggdendro)
-
-library(dendextend)
+# library(ggdendro)
+# 
+# library(dendextend)
 
 
 
@@ -203,7 +203,8 @@ lapply(stages, create_wordcloud)
 ############## HIERACHICAL CLUSTERING ################################
 
 colo_tweets <- colo_clusters %>%
-  filter(cluster == "1" | cluster == "2"| flood_stage == "Post_Flood") %>% 
+  # filter(cluster == "1" & flood_stage == "Post_Flood") %>% 
+  filter(cluster == "1") %>% 
   # filter(cluster == "1" | cluster == "2") %>% 
   st_set_geometry(NULL) %>% 
   rename(text = `tweet`) %>% 
@@ -222,12 +223,16 @@ colo_tokens <- colo_tweets %>%
   # filter(!str_detect(word, "[0-9]"), !str_detect(word, "#"))%>% 
   anti_join(stop_words)
 
-colo_dtm <- colo_tokens %>%
+# colo_dtm <- colo_tokens %>%
+#   count(document, word) %>%
+#   cast_tdm(word, document, n) # Check but I think dtm or tdm depends on the parameters' order.
+
+colo_dtm <- colo_tokens %>% ## CREO QUE ESTE ES EL QUE SIRVE
   count(document, word) %>%
-  cast_tdm(word, document, n) # Check but I think dtm or tdm depends on the parameters' order.
+  cast_dtm(document, word, n) # Check but I think dtm or tdm depends on the parameters' order.
 
 colo_dtm <- colo_dtm %>%  # Not sure if now I need this
-  removeSparseTerms(0.9975)
+  removeSparseTerms(0.9925)
 
 colo_matrix <- as.matrix(colo_dtm) #Defining TermDocumentMatrix as matrix
 colo_matrix <- colo_matrix[complete.cases(colo_matrix), ] #Not sure about this
@@ -238,10 +243,10 @@ d <- dist(colo_matrix, method="cosine")
 # See example here: http://www.dcc.fc.up.pt/~ltorgo/DM1_1718/Rclustering.html
 
 methds <- c('complete','average', 'single', 'ward.D', 'ward.D2')
-avgS <- matrix(NA,ncol=5,nrow=80,
-               dimnames=list(2:81,methds))
+avgS <- matrix(NA,ncol=5,nrow=10,
+               dimnames=list(2:11,methds))
 
-for(k in 2:81) 
+for(k in 2:11) 
   for(m in seq_along(methds)) {
     h <- hclust(d,meth=methds[m])
     c <- cutree(h,k)
@@ -265,8 +270,29 @@ plot(hc, main = "Hierarchical clustering of Reports on Colorado Floods",
      sub = "Colorado Clusters & Post-Flood",
      ylab = "", xlab = "", yaxt = "n", horiz = TRUE)
 
-rect.hclust(hc, 5, border = "red") #Play with that number and colors later
+rect.hclust(hc, 3, border = "red") #Play with that number and colors later
 
+clustering <- cutree(hc, 3)
 
+p_words <- colSums(colo_matrix) / sum(colo_matrix)
+
+cluster_words <- lapply(unique(clustering), function(x){
+  rows <- colo_matrix[ clustering == x , ]
+  
+  # for memory's sake, drop all words that don't appear in the cluster
+  rows <- rows[ , colSums(rows) > 0 ]
+  
+  colSums(rows) / sum(rows) - p_words[ colnames(rows) ]
+})
+
+cluster_summary <- data.frame(cluster = unique(clustering),
+                              size = as.numeric(table(clustering)),
+                              top_words = sapply(cluster_words, function(d){
+                                paste(
+                                  names(d)[ order(d, decreasing = TRUE) ][ 1:5 ], 
+                                  collapse = ", ")
+                              }),
+                              stringsAsFactors = FALSE)
+cluster_summary
 
 
