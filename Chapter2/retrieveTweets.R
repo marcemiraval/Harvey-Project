f025080c85a11db0bddab3d76b0846d7231be824
marcemiraval <marcela.suarez@gmail.com>
2019-01-13 21:44:44 -0800

    Function found online to retrieve the whole tweet based on tweetID

---
+++ Chapter2/retrieveTweets.R
@@ -0,0 +1,44 @@
+
+
+library(twitteR) 
+library(ROAuth)
+
+# Twitter authentication
+requestURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+consumerKey <-"Zx6QiuJQXTvrpkbHHPntox5Xs"
+consumerSecret <-"TnE9bKf7FeEla5X1rqjkHgOcWjBqL0lCoGNJeWKEYa3KsEPpE5"
+
+setup_twitter_oauth(consumerKey, consumerSecret, 
+                    access_token="274784775-9C4tOo68qtlGRJ56p7gJOQVj2kR3UhRfPHMSrpSS", 
+                    access_secret="9pXKEqQyY4MFbHtBhykB16SxYJBq0ENcXnPYWfJd4tDTO")
+
+lookupStatus <- function (ids, ...){
+  lapply(ids, twitteR:::check_id)
+  
+  batches <- split(ids, ceiling(seq_along(ids)/100))
+  
+  results <- lapply(batches, function(batch) {
+    params <- parseIDs(batch)
+    statuses <- twitteR:::twInterfaceObj$doAPICall(paste("statuses", "lookup", 
+                                                         sep = "/"),
+                                                   params = params, ...)
+    twitteR:::import_statuses(statuses)
+  })
+  return(unlist(results))
+}
+
+parseIDs <- function(ids){
+  id_list <- list()
+  if (length(ids) > 0) {
+    id_list$id <- paste(ids, collapse = ",")
+  }
+  return(id_list)
+}
+
+ids <- c("898676461032189952", "898679833554759680")
+tweets <- lookupStatus(ids, retryOnRateLimit=100)
+
+tweets_df <- twListToDF(tweets)
+
