1eb3fff53ec6bf76c4a0dc6525ba58fcf5c9bdc1
marcemiraval <amsuarez@umail.ucsb.edu>
2019-11-09 17:40:53 -0800

    Code to train topic models when considering only the affected counties and tweets sent during flood stage.

---
+++ Chapter1/Colorado/training_topic_models_flood_county.R
@@ -0,0 +1,28 @@
+library(stm) #fast compared to other implementations of topic models. Base don C++
+library(furrr)
+
+
+
+flood_county_tweet_sparse <- readRDS(file = "R:/Users/amsuarez/Colorado/flood_county_tweet_sparse.rds")
+
+
+
+plan(multiprocess)
+
+many_models <- data_frame(K = c(seq(3,24, by = 1))) %>%
+  mutate(topic_model = future_map(K, ~stm(flood_county_tweet_sparse, K = ., #colo_sparse
+                                          verbose = FALSE)))
+
+heldout <- make.heldout(flood_county_tweet_sparse)
+
+k_result_flood_county <- many_models %>%
+  mutate(exclusivity = map(topic_model, exclusivity),
+         semantic_coherence = map(topic_model, semanticCoherence, flood_county_tweet_sparse),
+         eval_heldout = map(topic_model, eval.heldout, heldout$missing),
+         residual = map(topic_model, checkResiduals, flood_county_tweet_sparse),
+         bound =  map_dbl(topic_model, function(x) max(x$convergence$bound)),
+         lfact = map_dbl(topic_model, function(x) lfactorial(x$settings$dim$K)),
+         lbound = bound + lfact,
+         iterations = map_dbl(topic_model, function(x) length(x$convergence$bound)))
+
+saveRDS(k_result_flood_county, file = "R:/Users/amsuarez/Colorado/k_result_flood_county.rds")
