3ccfe1398f0c8533ca0d0a1199aa5a384d06f309
marcemiraval <amsuarez@umail.ucsb.edu>
2019-11-19 11:17:39 -0800

    Updated arguments and parameters in scale_fill_manual so that the fuction works

--- Chapter1/Colorado/create_histo.R
+++ Chapter1/Colorado/create_histo.R
@@ -1,7 +1,7 @@
 
-create_histo = function(InputFile, HistoColor = NA, HistoBinWidth, HistoName, SavePath){
-  gen_hist <- ggplot(InputFile, aes(x = InputFile$date,  fill = InputFile$flood_stage)) +
-  geom_histogram(aes(y = ..count..), colour = HistoColor, binwidth = HistoBinWidth, 
+create_histo = function(InputFile, HistoColor = NA, HistoBinWidth, SavePathFilename){
+  gen_hist <- ggplot(InputFile, aes(x = InputFile$date,  fill = InputFile$stage)) +
+  geom_histogram(aes(y = ..count..), colour = HistoColor, binwidth = HistoBinWidth, # change to y = ..density.. if I want density
                  position="identity", alpha =0.9) +
   scale_x_datetime(name = "Date", breaks = date_breaks("2 day"),
                    labels = date_format("%m/%d"), expand = c(0.01,0),
@@ -16,12 +16,10 @@ create_histo = function(InputFile, HistoColor = NA, HistoBinWidth, HistoName, Sa
         axis.text.x=element_text(colour="#636363", size = 10),
         axis.text.y=element_text(colour="#636363", size = 10),
         legend.position = c(1, 0.7), legend.justification = c(1, 0))+
-  scale_fill_manual(values = c("#2ca25f", "#d53e4f","#fc8d59", "#3288bd","#fee08b"), 
-                    limits = c("Pre_flood", "Flood", "Immediate_Aftermath", "Post_Flood"),
-                    breaks = c("Pre_flood", "Flood", "Immediate_Aftermath", "Post_Flood"))+
+  scale_fill_manual(values = c("#2ca25f", "#d53e4f","#fc8d59", "#3288bd","#fee08b"))+ # In this order "Pre_flood", "Flood", "Immediate_Aftermath", "Post_Flood"
   labs(fill="") 
 
-  ggsave(filename = HistoName, path = SavePath, plot = gen_hist, device = "png",width = 40, height = 20,
+  ggsave(filename = SavePathFilename, plot = gen_hist, device = "png",width = 40, height = 20,
        units = "cm")
 }
 
