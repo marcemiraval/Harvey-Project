7fb1944dae9ffefac277123b1b3c10cfef0c2b80
marcemiraval <amsuarez@umail.ucsb.edu>
2019-11-29 12:46:02 -0800

    Updated information of counties to be used according to findings from previous papers using this case study.

--- Chapter1/Colorado/getting_affected_counties_Colorado.R
+++ Chapter1/Colorado/getting_affected_counties_Colorado.R
@@ -4,9 +4,15 @@ library(sf)
 # Data to create basemap
 counties <- counties("Colorado", cb = FALSE, resolution = "500k")
 counties_sf <- st_as_sf(counties)
+
 affected_counties <- counties_sf %>% 
-  filter(NAME == "Arapahoe" | NAME == "Boulder" | NAME == "Denver"|
-           NAME == "El Paso" | NAME ==  "Jefferson" | NAME ==  "Larimer" |
-           NAME == "Logan" | NAME ==  "Morgan" | NAME ==  "Weld")
+  dplyr::filter(NAME == "Adams" | NAME == "Arapahoe" | NAME == "Boulder" | 
+           NAME == "Clear Creek" | NAME == "Crowley" |NAME == "Denver"|
+           NAME == "El Paso" | NAME == "Fremont" | NAME == "Gilpin" |
+           NAME ==  "Jefferson" | NAME ==  "Lake" | NAME ==  "Larimer" |
+           NAME ==  "Lincoln" | NAME == "Logan" | NAME ==  "Morgan" | 
+           NAME ==  "Sedgwick" | NAME ==  "Washington" | NAME ==  "Weld") # 18 counties were designated for public assistance (FEMA).
 
 saveRDS(affected_counties, file = "affected_counties.rds")
+
+
