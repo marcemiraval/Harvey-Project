cd43df87473c0579caef6e1cba9d6018c2db8d1c
marcemiraval <amsuarez@umail.ucsb.edu>
2020-05-18 14:20:57 -0700

    Training topic model for flooded areas

---
+++ Chapter1/Colorado/training_topic_model_floodedAreas.R
@@ -0,0 +1,29 @@
+
+library(stm) #fast compared to other implementations of topic models. Base don C++
+library(furrr)
+
+
+
+tweetInFlood_sparse <- readRDS(file = "Chapter1/Colorado/tweetInFlood_sparse.rds")
+
+
+
+plan(multiprocess)
+
+many_models <- data_frame(K = c(seq(2, 20, by = 1))) %>%
+  mutate(topic_model = future_map(K, ~stm(tweetInFlood_sparse, K = ., #colo_sparse
+                                          verbose = FALSE)))
+
+heldout <- make.heldout(tweetInFlood_sparse)
+
+k_result_tweetInFlood <- many_models %>%
+  mutate(exclusivity = map(topic_model, exclusivity),
+         semantic_coherence = map(topic_model, semanticCoherence, tweetInFlood_sparse),
+         eval_heldout = map(topic_model, eval.heldout, heldout$missing),
+         residual = map(topic_model, checkResiduals, tweetInFlood_sparse),
+         bound =  map_dbl(topic_model, function(x) max(x$convergence$bound)),
+         lfact = map_dbl(topic_model, function(x) lfactorial(x$settings$dim$K)),
+         lbound = bound + lfact,
+         iterations = map_dbl(topic_model, function(x) length(x$convergence$bound)))
+
+saveRDS(k_result_tweetInFlood, file = "k_result_tweetInFlood.rds")
