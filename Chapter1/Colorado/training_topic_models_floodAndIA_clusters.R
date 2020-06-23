84a9d0e9eeeb1cd798fd5d9cd0f9e0c718f4eecb
marcemiraval <amsuarez@umail.ucsb.edu>
2020-06-22 20:47:04 -0700

    Update the code following warnings (multicore)

--- Chapter1/Colorado/training_topic_models_floodAndIA_clusters.R
+++ Chapter1/Colorado/training_topic_models_floodAndIA_clusters.R
@@ -2,13 +2,9 @@
 library(stm) #fast compared to other implementations of topic models. Base don C++
 library(furrr)
 
+clusters_floodAndIA_sparse <- readRDS(file = "Chapter1/Colorado/Data/clusters_floodAndIA_sparse.rds") #it didn't work with sparce just with dfm
 
-
-clusters_floodAndIA_sparse <- readRDS(file = "clusters_floodAndIA_sparse.rds") #it didn't work with sparce just with dfm
-
-clusters_floodAndIA_dfm <- readRDS(file = "clusters_floodAndIA_dfm.rds")
-
-plan("multiprocess")
+plan("multicore")
 
 many_models <- data_frame(K = c(seq(3,24, by = 1))) %>%
   mutate(topic_model = future_map(K, ~stm(clusters_floodAndIA_sparse, K = ., #colo_sparse
@@ -27,4 +23,4 @@ k_result_floodAndIA_clusters <- many_models %>%
          lbound = bound + lfact,
          iterations = map_dbl(topic_model, function(x) length(x$convergence$bound)))
 
-saveRDS(k_result_floodAndIA_clusters, file = "k_result_floodAndIA_clusters.rds")
+saveRDS(k_result_floodAndIA_clusters, file = "Chapter1/Colorado/Data/k_result_floodAndIA_clusters.rds")
